6f1391ad938c1e6c4feebc8e75ae164cdcd435d1
amit <tiwarya9@gmail.com>
2016-10-25 02:00:14 +0530

    Added map and user transaction(working)

---
+++ app/src/main/java/com/faseapp/faseapp/Merchantshop.java
@@ -0,0 +1,8 @@
+package com.faseapp.faseapp;
+
+/**
+ * Created by amit on 25/10/16.
+ */
+
+public class Merchantshop {
+}
---
+++ app/src/main/java/navigation/User_transaction.java
@@ -0,0 +1,8 @@
+package navigation;
+
+/**
+ * Created by amit on 25/10/16.
+ */
+
+public class User_transaction {
+}
