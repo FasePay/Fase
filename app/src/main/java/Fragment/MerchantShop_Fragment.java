a6fb7ad96976c3b3a9551fae9282dda2f3df405d
amit <tiwarya9@gmail.com>
2016-10-25 02:03:59 +0530

    Added map and user transaction(working)

--- app/src/main/java/Fragment/MerchantShop_Fragment.java
+++ app/src/main/java/Fragment/MerchantShop_Fragment.java
@@ -1,9 +1,88 @@
 package Fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.faseapp.faseapp.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by Sonu on 21-Oct-16.
  */
-public class MerchantShop_Fragment extends Fragment {
+public class MerchantShop_Fragment extends Fragment implements OnMapReadyCallback {
+    GoogleMap myMap;
+    MapView mMapView;
+
+    public MerchantShop_Fragment() {
+        super();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.merchantshop, container, false);
+        mMapView = (MapView) view.findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this);
+        return view;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+    @Override
+    public void onLowMemory()
+    {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng latLng=new LatLng(22.5965, 88.4176);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+        googleMap.addMarker(new MarkerOptions().position(latLng));
+    }
 }
--- app/src/main/java/com/faseapp/faseapp/Merchantshop.java
+++ app/src/main/java/com/faseapp/faseapp/Merchantshop.java
@@ -1,8 +1,81 @@
 package com.faseapp.faseapp;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by amit on 25/10/16.
  */
 
-public class Merchantshop {
+public class Merchantshop extends AppCompatActivity implements OnMapReadyCallback {
+    MapView mMapView;
+    private final String LOG_TAG = "FTAG";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.merchantshop);
+        mMapView = (MapView) findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+    @Override
+    public void onLowMemory()
+    {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        if (googleMap == null)
+            return; // Google Maps not available
+       // try {
+            MapsInitializer.initialize(this);
+      /*  }
+        catch (GooglePlayServicesNotAvailableException e) {
+            Log.e(LOG_TAG, "Have GoogleMap but then error", e);
+            return;
+        }*/
+        LatLng latLng=new LatLng(22.5965, 88.4176);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+        googleMap.addMarker(new MarkerOptions().position(latLng));
+
+    }
 }
