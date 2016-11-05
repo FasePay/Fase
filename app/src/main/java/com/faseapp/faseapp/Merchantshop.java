b99de07d27bb055d66e5118bdaa4e4cd32089319
FaseAppDeveloper <asussonu1994@gmail.com>
2016-11-05 15:21:32 +0530

    Map View

--- app/src/main/java/com/faseapp/faseapp/Merchantshop.java
+++ app/src/main/java/com/faseapp/faseapp/Merchantshop.java
@@ -1,15 +1,20 @@
 package com.faseapp.faseapp;
 
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -18,20 +23,59 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 /**
  * Created by amit on 25/10/16.
  */
 
 public class Merchantshop extends Fragment implements OnMapReadyCallback {
     MapView mMapView;
-    private final String LOG_TAG = "FTAG";
+    private GoogleMap mMap;
+    public EditText editText;
+//    private final String LOG_TAG = "FTAG";
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //super.onCreate(savedInstanceState);
-       // setContentView(R.layout.merchantshop);
-        View view=inflater.inflate(R.layout.merchantshop,container,false);
-        mMapView = (MapView)view. findViewById(R.id.map);
+        // setContentView(R.layout.merchantshop);
+        View view = inflater.inflate(R.layout.merchantshop, container, false);
+        mMapView = (MapView) view.findViewById(R.id.map);
         mMapView.onCreate(savedInstanceState);
+        editText=(EditText) view.findViewById(R.id.TFaddress);
+        ImageButton btn=(ImageButton) view.findViewById(R.id.button);
+        Button btn3=(Button)view.findViewById(R.id.zoomin);
+        Button btn4=(Button)view.findViewById(R.id.zoomout);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String location=editText.getText().toString();
+                List<Address> addressList=null;
+                if(location!=null || !location.equals(""))
+                {
+                    Geocoder geocoder=new Geocoder(getApplicationContext());
+                    try {
+                        addressList= geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    Address address=addressList.get(0);
+                    LatLng latLng=new LatLng(address.getLatitude(),address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng)) ;
+                }
+            }
+
+
+
+        });
+
         mMapView.getMapAsync(this);
         return view;
     }
@@ -50,17 +94,17 @@ public class Merchantshop extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState)
-    {
+    public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mMapView.onSaveInstanceState(outState);
     }
+
     @Override
-    public void onLowMemory()
-    {
+    public void onLowMemory() {
         super.onLowMemory();
         mMapView.onLowMemory();
     }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -71,16 +115,22 @@ public class Merchantshop extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         if (googleMap == null)
             return; // Google Maps not available
-       // try {
-            MapsInitializer.initialize(getActivity());
+        // try {
+        MapsInitializer.initialize(getActivity());
       /*  }
         catch (GooglePlayServicesNotAvailableException e) {
             Log.e(LOG_TAG, "Have GoogleMap but then error", e);
             return;
         }*/
-        LatLng latLng=new LatLng(22.5965, 88.4176);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
-        googleMap.addMarker(new MarkerOptions().position(latLng));
+
+    }
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            return;
+        }
 
     }
 }
